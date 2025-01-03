a1156f27323115a7f87755531118404772f1bbf0
Kazem Gholian <kgholian@gmail.com>
2025-01-03 17:36:15 +0330

    new liara changes

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3005;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
