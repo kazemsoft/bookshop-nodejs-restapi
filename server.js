09da26abd44ad04325216dbe3ac4fa277429e63d
Kazem Gholian <kgholian@gmail.com>
2025-01-03 17:52:53 +0330

    listen on 0.0.0.0

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const PORT = process.env.PORT || 3005;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
