6ebaeb44cb75b6e953fde4c597dc993d02e3ee84
Kazem Gholian <kgholian@gmail.com>
2025-01-03 18:01:28 +0330

    .env

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
